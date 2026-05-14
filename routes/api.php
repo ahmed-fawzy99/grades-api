fb1a5494eaab5e4e1b1c541b57b2b70196f710fe
Ahmed Deghady <52136140+ahmed-fawzy99@users.noreply.github.com>
2026-05-14 14:14:55 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,10 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
+    Route::get('/', function () {
+        return response()->json([
+            'message' => 'Welcome to the API v1',
+        ]);
+    });
     Route::apiResource('grades', GradeController::class)->only(['index', 'store', 'show', 'destroy']);
 });
